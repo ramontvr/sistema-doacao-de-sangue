bed15019d3b576d96f0d2c5f70512bdf327adb90
Ramon Tavares <rasilva8@gmail.com>
2020-02-18 17:39:58 -0300

    Adição do nunjucks

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,36 @@
 
 const express = require('express')
+
+
+const app = express()
+
+//CONFIGURAR SERVIDOR PRA APRESENTAR ARQUIVOS EXTRAS
+app.use(express.static('../frontend/public'))
+
+//CONFIGURAÇÃO DA TMEPALTE ENGINE
+const nunjucks = require('nunjucks')
+nunjucks.configure("../frontend", {
+    express: app,
+    noCache: true,
+})
+
+const donors = [
+        {
+            name: "Ramon Tavares",
+            blood: "O+"
+
+        }
+]
+
+
+//RENDERIZANDO A PÁGINA
+app.get('/', function(req,res) {
+
+    return res.render('index.html', {donors})    
+})
+
+
+
+app.listen(3333, () => {
+    console.log('Servidor iniciado')
+})
